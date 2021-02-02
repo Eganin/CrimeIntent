5dee641265fc9456ea15afb2d85c30dffcc08d90
Eganin <egorzaharin12@gmail.com>
2021-02-02 15:15:37 +0300

    init project

---
+++ app/src/main/java/com/example/crimeintent/ui/screens/criminal/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.crimeintent.ui.screens.criminal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.crimeintent.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
